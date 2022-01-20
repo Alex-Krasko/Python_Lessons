6845c4605322ebe3aaff9c98a5c93f084e2653da
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2022-01-20 21:25:57 +0300

    Lesson2 edited

--- Lesson2/Lesson2.py
+++ Lesson2/Lesson2.py
@@ -8,7 +8,33 @@ def max_of_three(x,y,z):
 import random
 
 # print(random.randint(1,22))
+def Input():
+    n = int(input("Введите число: "))
+    return n
+# print(a%7==0 & a%23==0)
 
-a = int(input("Введите число: "))
-print(a%7==0 & a%23==0)
+# for i in range(-n, n+1):
+#    print(i)
+
+# a=Input()
+# b=Input()
+# print(a*a==b or b*b==a)
+
+def Square(n):
+    for i in range(1,n+1):
+        print(i,i*i)
+
+def Coub(n):
+    for i in range(1,n+1):
+        print(i,i*i*i)
+
+def Coub_even(n):
+    for i in range(1,n+1):
+        a=i*i*i
+        if a%2==0: print(a)
+
+# n=Input()
+# Square(n)
+# Coub(n)
+# Coub_even(n)
 
