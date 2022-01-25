177f1cdc6e8f5d1045321bd045ff96cd1df33bea
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2022-01-25 19:32:16 +0300

    Lesson3 edited

---
+++ Lesson3/Lesson3.py
@@ -0,0 +1,20 @@
+# colors = ['red', 'green', 'blue123']
+# data = open('file.txt', 'w')  # Работа с файлами
+# data.writelines(colors)
+# data.write('\nline 1\n')
+# data.close()
+
+# # exit()
+
+# with open('file.txt', 'w') as data: # То же, но с алиасами
+#     data.writelines(colors)
+#     data.write('\nline 1\n')
+
+# dictionary = {}   # Словари
+# dictionary = \
+#     {
+#         'up': 'ВВЕРХ',
+#         'down': 'ВНИЗ',
+#         'left': 'ВЛЕВО',
+#         'right': 'ВПРАВО'
+#     }
